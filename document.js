d3a570f897b3783ff708686f78afc802fca404ca
RicMejiaZ <121823489+RicMejiaZ@users.noreply.github.com>
2026-05-22 13:05:16 -0600

    Update download button event listener for PDF

--- document.js
+++ document.js
@@ -1,9 +1,12 @@
 //Attach the document Ricardo_Mejia.pdf to the download link
 
-document.getElementById("dl").addEventListener("click", () => {
+document.getElementById("downloadBtn").addEventListener("click", () => {
+	// Aquí irá tu lógica de descarga
+	const url =
+		"https://ricmejiaz.github.io/Git_project/document/Ricardo_Mejia.pdf";
 	const a = document.createElement("a");
-	a.href = "https://ricmejiaz.github.io/Git_project/document/Ricardo_Mejia.pdf";
-	a.setAttribute("download", "Ricardo_Mejia.pdf"); // suggested filename
+	a.href = url;
+	a.setAttribute("download", "Ricardo_Mejia.pdf");
 	document.body.appendChild(a);
 	a.click();
 	document.body.removeChild(a);
