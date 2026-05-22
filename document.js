79c7454cead98b9e44de6c503b61cb36cff8ddea
RicMejiaZ <121823489+RicMejiaZ@users.noreply.github.com>
2026-05-22 13:22:21 -0600

    Change download link to use raw GitHub URL

--- document.js
+++ document.js
@@ -1,11 +1,8 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
-  const repo = "ricmejiaz/Git_project"; // tu usuario/repositorio
-  const path = "document/Ricardo_Mejia.pdf"; // ruta dentro del repo
-  const branch = "main"; // o la rama que usas
-
-  const cdnUrl = `https://cdn.jsdelivr.net/gh/${repo}@${branch}/${path}`;
+  const rawUrl =
+    "https://raw.githubusercontent.com/ricmejiaz/Git_project/main/document/Ricardo_Mejia.pdf";
   const a = document.createElement("a");
-  a.href = cdnUrl;
+  a.href = rawUrl;
   a.setAttribute("download", "Ricardo_Mejia.pdf");
   document.body.appendChild(a);
   a.click();
