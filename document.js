d6f8e946e30a0d36cd11478e6d6f4afa80948f68
RicMejiaZ <121823489+RicMejiaZ@users.noreply.github.com>
2026-05-22 13:38:15 -0600

    Update document.js

--- document.js
+++ document.js
@@ -1,6 +1,6 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
   const rawUrl =
-    "https://raw.githubusercontent.com/ricmejiaz/Git_project/master/document/Ricardo_Mejia.pdf";
+    "https://raw.githubusercontent.com/RicMejiaZ/Git_project/master/document/Ricardo_Mejia.pdf";
   const a = document.createElement("a");
   a.href = rawUrl;
   a.setAttribute("download", "Ricardo_Mejia.pdf");
